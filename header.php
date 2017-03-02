5900ff741644eea3b42078a01b7793a6d4134a9d
litaylor <litaylor@live.unc.edu>
2017-03-01 23:40:22 -0500

    Set up bootstrap containers.

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   if (!ini_get('display_errors')) {
     ini_set('display_errors', '1');
   }
+
   $db = new PDO('mysql:host=localhost;dbname=contacts;charset=utf8mb4','root','root');
 ?>
 
@@ -10,5 +11,10 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>This is the header.</title>
+    <title>Contact manager</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css?<?php echo time(); ?>">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
--- index.php
+++ index.php
@@ -1,8 +1,42 @@
 <?php
-  include 'header.php'
+  include 'header.php';
+  $contacts = $db->query('SELECT * FROM contacts')->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <body>
+  <div class="container-fluid" data-spy="affix" data-offset-top='0'>
+    <div class="row">
+      <div class="col-xs-12 col-md-6 col-md-offset-3">
+        <h1>My contacts <span class="text-muted">(<?= count($contacts); ?>)</span></h1>
+      </div>
+    </div>
+    <div class="row">
+      <a href="add.php">
+        <div class="col-xs-12 col-md-6 col-md-offset-3">
+          <h3>Add New +1</h3>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="container-fluid contact-list">
+    <div class="row">
+      <?php foreach($contacts as $contact) : ?>
+        <div class="col-xs-12 col-md-6 col-md-offset-3">
+          <div class="row contact">
+            <div class="col-xs-9">
+              <?= $contact['firstname']; ?> <?= $contact['lastname']; ?>
+            </div>
+            <div class="col-xs-3">
+              Call
+            </div>
+          </div>
 
+        </div>
+      <?php endforeach ?>
+    </div>
+  </div>
+  <div style="background-color:gray; width:100%; height:900px;">
+
+  </div>
 </body>
 </html>
