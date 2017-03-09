5cc3c9e07ba093caf4d74702c0745cf7b6ff9793
litaylor <litaylor@live.unc.edu>
2017-03-09 17:30:28 -0500

    Renamed style guide.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="row footer">
   <div class="col-xs-12 col-sm-6 col-sm-offset-3 footer">
-    <h3>footer</h3>
+    <p>Designed by Langston Taylor. | Style Guide</p>
   </div>
 </div>
 </div>
@@ -17,7 +17,8 @@
   });
 
   $(".plain .hoverglyph, #optionedit").click(function(){
-    $(".plain").removeClass("plain").addClass("editing")
+    $(".plain").removeClass("plain").addClass("editing");
+    $(".options").slideToggle();
   });
 
   $(".plain .hoverglyph").click(function(){
